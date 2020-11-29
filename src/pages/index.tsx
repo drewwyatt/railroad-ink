a42018701feb2e7af591417aff21e859ddd572d5
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-11-28 23:51:38 -0500

    board grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,40 @@
+import { times } from 'ramda'
 import type { FC } from 'react'
 
-const Page: FC = () => <h1>Railroad Ink</h1>
+const Cell: FC<{ idx: number }> = ({ idx, ...props }) => (
+  <div {...props}>
+    <button>Space {idx}</button>
+    <style jsx>{`
+      button {
+        width: 100px;
+        height: 100px;
+      }
+    `}</style>
+  </div>
+)
+
+const cell = (idx: number) => <Cell idx={idx} key={['space', idx].join('-')} />
+
+const Page: FC = () => (
+  <>
+    <main>
+      <h1>Railroad Ink</h1>
+      <article>{times(cell, 7 * 7)}</article>
+    </main>
+    <style jsx>{`
+      main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+      }
+      article {
+        display: grid;
+        grid-template-columns: repeat(7, 100px);
+      }
+    `}</style>
+  </>
+)
 export default Page
