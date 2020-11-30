000efb369df872acb293542a1e664b5828ff69e6
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-11-29 22:28:41 -0500

    add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import type { FC } from 'react'
 import '../styles.css'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default App
