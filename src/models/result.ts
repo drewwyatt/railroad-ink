8715bcc8021d6d61133df2ccf5a4b078281f34e5
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-11-30 00:34:20 -0500

    add type guards

--- src/models/result.ts
+++ src/models/result.ts
@@ -18,3 +18,12 @@ export type PendingResult<OK, Err = unknown> = Result<OK, Err> | Pending
 export const ok = <T>(value: T) => ({ type: 'OK', value } as OKType<T>)
 export const error = <T>(value: T) => ({ type: 'ERROR', value } as ErrorType<T>)
 export const pending: Pending = { type: 'PENDING' }
+
+export const isOK = <T>(result: PendingResult<T, any>): result is OKType<T> =>
+  result.type === 'OK'
+
+export const isError = <T>(result: PendingResult<any, T>): result is ErrorType<T> =>
+  result.type === 'ERROR'
+
+export const isPending = (result: PendingResult<any, any>): result is Pending =>
+  result === pending
