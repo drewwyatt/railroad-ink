0866929ec09bc660767f25c02b9975d3410d2c3a
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-21 23:04:01 -0500

    prevent double placement of rolled routes

--- src/components/contexts/turn/reducer.ts
+++ src/components/contexts/turn/reducer.ts
@@ -1,20 +1,30 @@
 import { update } from 'ramda'
 import type { Reducer } from 'react'
-import { ok, pending } from '~/models/result'
+import { ok, pending, isOK } from '~/models/result'
 import Turn, { DEFAULT_VALUE } from '~/models/turn'
 import * as actions from './actions'
 
 export type State = Turn
 export type Action = ReturnType<typeof actions[keyof typeof actions]>
 
+const hasBoardIndex = (idx: number) => (result: State[number]) =>
+  isOK(result) && result.value[0] === idx
+
+const applyMove = (state: State, action: Extract<Action, { type: 'move' }>): State => {
+  const existingIdx = state.findIndex(hasBoardIndex(action.payload.boardIndex))
+  console.log('apply existing', existingIdx)
+  const moveableState = existingIdx === -1 ? state : update(existingIdx, pending, state)
+  return update(
+    action.payload.rollIndex,
+    ok([action.payload.boardIndex, action.payload.attributes]),
+    moveableState,
+  ) as Turn
+}
+
 const reducer: Reducer<State, Action> = (state, action) => {
   switch (action.type) {
     case 'move':
-      return update(
-        action.payload.rollIndex,
-        ok([action.payload.boardIndex, action.payload.attributes]),
-        state,
-      ) as Turn
+      return applyMove(state, action)
     case 'reset':
       return DEFAULT_VALUE
     case 'undo':
